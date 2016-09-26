2ebe30d77b62721d11eb4c0e253481ee18bb2a43
Tatiana <ttishchenko1@gmail.com>
2016-09-26 13:00:08 +0300

    initial commit

---
+++ readWrite/readFile.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var allGames, 
+	qtyWins = 0, 
+	qtyLoss = 0, 
+	maxWins = 0, 
+	maxLoss = 0,
+	flagWin;
+
+fs.readFile('log.txt', 'utf8',(err, data) => {
+
+	if (err) throw err;
+	allGames = data.split('\r');
+	// allGames = allGames.trim();
+	allGames = allGames.filter(function(row) {
+		return row.length > 0;
+	});
+	allGames.forEach( function (items) {
+		var item = items.split('=');
+
+		// console.log(item);
+
+		if (item[0] == item[1]) {
+			if(flagWin) {
+				maxWins ++;
+			}
+			qtyWins = qtyWins + 1;
+			flagWin = true;
+		} else {
+			if(!flagWin) {
+				maxLoss ++;
+			}
+			qtyLoss = qtyLoss + 1;
+			flagWin = false;
+		}
+
+	})
+
+    console.log('1. общее количество партий: ' + allGames.length);
+	console.log('2. общее выигранных партий: ' + qtyWins);
+	console.log('3. общее проигранных партий: ' + qtyLoss);
+	console.log('4. максимальное число побед подряд: ' + maxWins);
+	console.log('5. максимальное число проиграшей подряд: ' + maxLoss);
+
+});
---
+++ readWrite/writeFile.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var ask = require('prompt-autocomplete');
+var pcValue = Math.floor(Math.random() * 2) + 1;
+var states = [
+        '1',
+        '2'
+    ];
+var result;
+ 
+ask("Pick a number:", states, function (err, answer) {
+
+    if(answer == pcValue) {
+    	result = 'Угадали!';
+    } else {
+    	result = 'Попробуй еще!'
+    };
+
+	console.log(result);
+    console.log("Вы выбрали: " + answer);
+    console.log("Компьютер выбрал: " + pcValue);
+
+    fs.appendFile('log.txt', (answer + '='+ pcValue + '\r'), (err) => {
+		if (err) throw err;
+	});
+
+});
