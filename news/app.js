a675e5ed2eb962685dbd08196af486176c90d6e6
ttishchenko1 <ttishchenko1@gmail.com>
2016-09-27 11:46:57 +0300

    code formatting app.js

--- news/app.js
+++ news/app.js
@@ -1,56 +1,46 @@
-
 var request = require('request');
 var cheerio = require('cheerio');
 var express = require('express');
-var cons = require('consolidate'); // модуль для шаблонов
+var cons = require('consolidate');
 var app = express();
 var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
-
 app.engine('hbs', cons.handlebars);
  
-// set .html as the default extension 
+// set .hbs as the default extension 
 app.set('view engine', 'hbs');
-app.set('views', __dirname); //+ '/views'
-
-app.use(bodyParser.urlencoded({ extended: false }));
+app.set('views', __dirname);
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
 	res.render('index', {title:'Выбрать сайт:', news:[]});
 });
-app.post('/', function (req, res) {
-	
+
+app.post('/', function (req, res) {	
 	var getUrl = req.body.url;
 	var getQty = Number(req.body.qty);
 	var article;
 	var data = [];
+	
 	request(getUrl, function (error, response, body) {
-
-
 		if (!error && response.statusCode == 200) {
 			$ = cheerio.load(body);
-
 			if (getUrl === 'http://censor.net.ua/news/all') {
 				article = $('.anounce a');
 			} else if (getUrl === 'https://habrahabr.ru/top/') {
 				article = $('.posts .content');				
-			}; 
-			
+			}; 			
 			for (var i = 1; i < getQty+1; i++) {
 				data.push(i + '. ' +article.eq(i).text());
 			}
 			res.render('index', {title:getUrl, qtyArticle:getQty, news:data});
-
 		} else {
 			console.log('Status code: ', response.statusCode);
 		}
 	});
-
-
-
+	
 });
-
 
 app.listen(8080);
 console.log('Express server listening on port 8080');
